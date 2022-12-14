b511e2f47e248959a2408118225b6397be0753e3
JxoLus <xolus@naver.com>
2022-12-14 22:29:32 +0900

    commit1

---
+++ FactorialCalculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class FactorialCalculatorV1
+{
+  
+  private int number = 0;
+  private int result = 1;
+  
+  public static void main (String[] args)
+  {
+    
+    // xoLus FactorialCalculatorV1
+    System.out.println("   -----xoLus-----\nFactorialCalculatorV1");
+    
+    // operate
+    FactorialCalculatorV1 operator = new FactorialCalculatorV1();
+    operator.enter();
+    operator.calculation();
+    
+  }
+  
+  private void calculation()
+  {
+    
+    for (int i = 1; this.number >= i; i++)
+    {
+      
+      this.result = this.result * i;
+      
+    }
+    
+    System.out.println(this.number + "! = " + this.result);
+    
+  }
+  
+  private void enter()
+  {
+    
+    // get number
+    System.out.println("Enter Number");
+    Scanner sc = new Scanner(System.in);
+    this.number = sc.nextInt();
+    sc.close();
+    
+   }
+  
+}
